72b8f847b2a8daed63c7b1eaac7635ab6fb207ac
Koralez <Koralez@192.168.1.102>
2017-01-23 12:44:27 +0100

    first changes

---
+++ src/Testowa.java
@@ -0,0 +1,14 @@
+
+public class Testowa {
+
+	public static void main(String[] args) {
+		
+		int a=3;
+		
+		String string = Integer.toBinaryString(a);
+		
+		System.out.println(string);
+
+	}
+
+}
